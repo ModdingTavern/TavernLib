9d2d3e3c3f917b3431cb52091892d7c797d6c4e8
Ethan Barron <ethanbarron@proton.me>
2026-06-30 18:31:32 +0100

    Establish quick setup

--- TavernLib/Main.cs
+++ TavernLib/Main.cs
@@ -1,7 +1,19 @@
+using MelonLoader;
+using MelonLoader.Logging;
+
+
+[assembly: MelonInfo(typeof(TavernLib.Main), "TavernLib", "0.0.1", "Tavern Team", "https://github.com/ModdingTavern/TavernLib")]
 namespace TavernLib
 {
-    public class Main
+    public class Main : MelonPlugin
     {
+        internal static MelonLogger.Instance Logger { get; private set; }
         
+        
+        public override void OnEarlyInitializeMelon()
+        {
+            Logger = LoggerInstance;
+            Logger.MsgPastel(ColorARGB.Azure, "Hello from TavernLib!");
+        }
     }
 }
