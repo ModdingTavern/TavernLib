0cc25ce6fa430555138c174283f25a1116272c43
Ethan Barron <ethanbarron@proton.me>
2026-07-03 17:36:56 +0100

    Add automatic IP detection for server file generation

--- TavernLib/Debugging/DebugHelper.cs
+++ TavernLib/Debugging/DebugHelper.cs
@@ -1,4 +1,7 @@
 ﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Alta.Api.DataTransferModels.Models.Responses;
 using MelonLoader;
 using TavernLib.ServerBrowser;
@@ -17,20 +20,19 @@ namespace TavernLib.Debugging
         }
 
         private string _name, _description, _ipAddress;
+        private bool _automaticIpAddress;
 
         private void OnGui()
         {
             _name = GUILayout.TextField(_name);
             _description = GUILayout.TextField(_description);
-            _ipAddress = GUILayout.TextField(_ipAddress);
+            _automaticIpAddress = GUILayout.Toggle(_automaticIpAddress, "Automatically detect IP");
+            if (!_automaticIpAddress) _ipAddress = GUILayout.TextField(_ipAddress);
+            
             
             if (GUILayout.Button("Serialize"))
             {
-                var serverInfo = DevGameServerInfo.GetDevServer(_ipAddress, 1757, 0);
-                serverInfo.Description = _description;
-                serverInfo.Name = _name;
-
-                new CustomServerReference(_name, false).Serialize(serverInfo);
+                _ = SerializeServer();
             }
             
             
@@ -39,6 +41,26 @@ namespace TavernLib.Debugging
                 var pair = _logCatcher.Target.LoggingLevels.ElementAt(i);
                 _logCatcher.Target.LoggingLevels[pair.Key] = GUILayout.Toggle(pair.Value, pair.Key);
             }
+        }
+        
+        private async Task SerializeServer()
+        {
+            string ip;
+
+            if (!_automaticIpAddress) ip = _ipAddress;
+            else
+            {
+                using var httpClient = new HttpClient();
+                var result = await httpClient.GetStringAsync("https://icanhazip.com");
+                result = result.Replace("\\r\\n", "").Replace("\\n", "").Trim();
+                ip = result;
+            }
+            
+            var serverInfo = DevGameServerInfo.GetDevServer(ip, 1757, 0);
+            serverInfo.Description = _description;
+            serverInfo.Name = _name;
+
+            new CustomServerReference(_name, false).Serialize(serverInfo);
         }
     }
 }
