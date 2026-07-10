16350ef4a737926edfed60599c52b881097b6e55
Ethan Barron <ethanbarron@proton.me>
2026-07-10 19:07:06 +0100

    Implement first security patch

---
+++ TavernLib/Patches/SecurityPatch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Alta.Networking;
+using Alta.Networking.Servers;
+using Alta.Serialization;
+using HarmonyLib;
+
+namespace TavernLib.Patches
+{
+    [HarmonyPatch]
+    public static class SecurityPatch
+    {
+        [HarmonyPatch(typeof(ServerPlayerConnectionHandlerOld), nameof(ServerPlayerConnectionHandlerOld)), HarmonyPrefix]
+        public static bool RejectFlyCam(Connection connection, Stream stream, ServerPlayerConnectionHandlerOld __instance)
+        {
+            try
+            {
+                using var readingStream = stream.Clone() as Stream;
+
+                var requestJoinMessage = new RequestJoinMessage();
+                requestJoinMessage.Serialize(connection, stream);
+
+                if (requestJoinMessage.PlayerMode is PlayerMode.Fly or PlayerMode.AutoCam or PlayerMode.Unassigned)
+                {
+                    _ = ServerPlayerConnectionHandlerOld.PlayerDenied(connection, "Bizarre mode detected.");
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                Tavern.Logger.Error($"Error in SecurityPatch {e}");
+                _ = ServerPlayerConnectionHandlerOld.PlayerDenied(connection, "Unhandled error.");
+                throw;
+            }
+
+            return true;
+        }
+    }
+}
