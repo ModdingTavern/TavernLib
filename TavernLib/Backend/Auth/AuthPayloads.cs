0c835cc1a4f6a820e4658af6fb7ed40692fb38fa
Ethan Barron <ethanbarron@proton.me>
2026-07-20 07:34:13 +0100

    Change PingResponse to use a primary constructor

--- TavernLib/Backend/Auth/AuthPayloads.cs
+++ TavernLib/Backend/Auth/AuthPayloads.cs
@@ -12,21 +12,13 @@ namespace TavernLib.Backend.Auth
         }
         
         
-        public struct PingResponse
+        public struct PingResponse(string serverName, bool passwordRequired, bool whitelistEnabled, int gamePort)
         {
             [JsonProperty(PropertyName = "status")] private string Pong => "pong";
-            [JsonProperty(PropertyName = "server_name")] private string ServerName { get; set; }
-            [JsonProperty(PropertyName = "password_required")] private bool PasswordRequired { get; set; }
-            [JsonProperty(PropertyName = "whitelist_enabled")] private bool WhitelistEnabled { get; set; }
-            [JsonProperty(PropertyName = "game_port")] private int GamePort { get; set; }
-            
-            public PingResponse(string serverName, bool passwordRequired, bool whitelistEnabled, int gamePort)
-            {
-                ServerName = serverName;
-                PasswordRequired = passwordRequired;
-                WhitelistEnabled = whitelistEnabled;
-                GamePort = gamePort;
-            }
+            [JsonProperty(PropertyName = "server_name")] private string ServerName { get; set; } = serverName;
+            [JsonProperty(PropertyName = "password_required")] private bool PasswordRequired { get; set; } = passwordRequired;
+            [JsonProperty(PropertyName = "whitelist_enabled")] private bool WhitelistEnabled { get; set; } = whitelistEnabled;
+            [JsonProperty(PropertyName = "game_port")] private int GamePort { get; set; } = gamePort;
         }
 
 
