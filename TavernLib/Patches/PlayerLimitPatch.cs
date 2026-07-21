004500f320a2b660af44960609243c3686313067
Ethan Barron <ethanbarron@proton.me>
2026-07-21 12:21:49 +0100

    Align server player limit with config

---
+++ TavernLib/Patches/PlayerLimitPatch.cs
@@ -0,0 +1,16 @@
+﻿using Alta.Networking.Servers;
+using HarmonyLib;
+using TavernLib.Backend.Api;
+using TavernLib.Services;
+
+namespace TavernLib.Patches;
+
+[HarmonyPatch]
+public class PlayerLimitPatch
+{
+    [HarmonyPatch(typeof(ServerHandler), nameof(ServerHandler.PlayerLimit), MethodType.Getter), HarmonyPostfix]
+    public static void SetPlayerLimit(ref int __result)
+    {
+        __result = TavernServices.GetService<TavernApiManager>().ServerConfig.LastRead?.MaxPlayers ?? 0;
+    }
+}
