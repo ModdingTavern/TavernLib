78f8f85d798aaec9408a5e4ef0d433e8d16cac9b
Ethan Barron <ethanbarron@proton.me>
2026-07-21 10:21:13 +0100

    Update server payload

--- TavernLib/Backend/Server/ServerListingPayload.cs
+++ TavernLib/Backend/Server/ServerListingPayload.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using Alta.Networking.Servers;
+using Newtonsoft.Json;
 using TavernLib.Backend.Server.Configs;
 
 namespace TavernLib.Backend.Server
@@ -10,6 +11,8 @@ namespace TavernLib.Backend.Server
         [JsonProperty(PropertyName = "port")] public ushort Port { get; private set; }
         [JsonProperty(PropertyName = "player_limit")] public uint PlayerLimit { get; private set; }
         [JsonProperty(PropertyName = "has_password")] public bool HasPassword { get; private set; }
+        [JsonProperty(PropertyName = "player_count")] public int PlayerCount { get; private set; }
+        [JsonProperty(PropertyName = "community_listed")] public bool CommunityListed { get; private set; }
         
         public static ServerListingPayload FromConfig(ServerSettingsConfig config)
         {
@@ -19,7 +22,9 @@ namespace TavernLib.Backend.Server
                 Name = config.LastRead.Name,
                 Port = 1757, // TODO
                 PlayerLimit = config.LastRead.MaxPlayers,
-                HasPassword = !string.IsNullOrWhiteSpace(config.LastRead.PasswordHash)
+                HasPassword = !string.IsNullOrWhiteSpace(config.LastRead.PasswordHash),
+                PlayerCount = 5,
+                CommunityListed = config.LastRead.CommunityListed
             };
         }
     }
