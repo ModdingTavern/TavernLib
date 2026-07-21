6527a381e210bb37bd860a519b3b49b35351ec85
Ethan Barron <ethanbarron@proton.me>
2026-07-21 11:04:20 +0100

    Set non-primitive defaults

--- TavernLib/Backend/Server/Configs/ServerSettingsConfig.cs
+++ TavernLib/Backend/Server/Configs/ServerSettingsConfig.cs
@@ -4,15 +4,13 @@ namespace TavernLib.Backend.Server.Configs;
 
 public class ServerSettings
 {
-    [JsonProperty(PropertyName = "name")] public string Name { get; private set; }
-    [JsonProperty(PropertyName = "password_hash")] public string PasswordHash { get; private set; }
+    [JsonProperty(PropertyName = "name")] public string Name { get; private set; } = "My Tavern Server";
+    [JsonProperty(PropertyName = "password_hash")] public string PasswordHash { get; private set; } = "";
     [JsonProperty(PropertyName = "whitelist_enabled")] public bool WhitelistEnabled { get; private set; }
+    [JsonProperty(PropertyName = "enforce_ip_limit")] public bool EnforceIpLimit { get; private set; }
     [JsonProperty(PropertyName = "community_listed")] public bool CommunityListed { get; private set; }
     [JsonProperty(PropertyName = "max_players")] public uint MaxPlayers { get; private set; }
-    [JsonProperty(PropertyName = "community_listing_token")] public string CommunityListingToken { get; private set; }
+    [JsonProperty(PropertyName = "community_listing_token")] public string CommunityListingToken { get; private set; } = "";
 }
 
-public class ServerSettingsConfig(string filePath) : ServerConfigFile<ServerSettings>(filePath)
-{
-        
-}
+public class ServerSettingsConfig(string filePath) : ServerConfigFile<ServerSettings>(filePath);
