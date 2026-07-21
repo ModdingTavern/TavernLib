8c2f147baf39ed9ced773a38bd3b292cac0a2874
Ethan Barron <ethanbarron@proton.me>
2026-07-21 07:41:27 +0100

    Implement algorithm for storing client hash

--- TavernLib/Backend/BackendUtils.cs
+++ TavernLib/Backend/BackendUtils.cs
@@ -1,8 +1,20 @@
-﻿namespace TavernLib.Backend
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TavernLib.Backend
 {
     public static class BackendUtils
     {
         public static string TavernApi => "http://themoddingtavern.com:1763";
         public static string ServerUri => "/servers";
+
+
+        public static string HashClient(string input)
+        {
+            using var hash = SHA256.Create();
+            var byteArray = hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+            return BitConverter.ToString(byteArray).Replace("-", "").ToLower();
+        }
     }
 }
