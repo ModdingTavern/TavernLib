2c7abf1145125edb95f22b3c9c83b941e73846bf
Ethan Barron <ethanbarron@proton.me>
2026-07-22 08:58:41 +0100

    Remove unnecessary try catch, always deny result if error occured

--- TavernLib/Patches/SecurityPatch.cs
+++ TavernLib/Patches/SecurityPatch.cs
@@ -17,59 +17,58 @@ namespace TavernLib.Patches
             connection.SetHandler(MessageType.RequestJoin, FilterJoinRequest);
             return false;
         }
-        
+
         private static async void FilterJoinRequest(Connection connection, Stream stream)
         {
             Tavern.Logger.Msg(ColorARGB.Chartreuse, $"Filtering join request for user at IP {connection.IpAddress}");
-            
+
             try
             {
                 Tavern.Logger.Msg(ColorARGB.Chartreuse, "Filtering flycam joiners");
                 if (!await FilterFlyCam(connection, stream)) return;
-                
+
                 Tavern.Logger.Msg("User passed flycam check, checking for valid account token");
-                
+                if (!await FilterInvalidTokens(connection, stream)) return;
+
+                Tavern.Logger.Msg("User passed token check, moving onto vanilla check");
+                ServerHandler.Current.playerJoinHandler.CheckApproved(connection, stream);
             }
             catch (Exception e)
             {
                 Tavern.Logger.Error($"Error when filtering join request! {e}");
+                await ServerPlayerConnectionHandlerOld.PlayerDenied(connection, "Error when checking authenticity");
                 throw;
             }
         }
-        
+
+        private static async Task<bool> FilterInvalidTokens(Connection connection, Stream stream)
+        {
+        }
+
         private static async Task<bool> FilterFlyCam(Connection connection, Stream stream)
         {
             try
             {
-                try
-                {
-                    using var readingStream = stream.Clone() as Stream;
+                using var readingStream = stream.Clone() as Stream;
 
-                    var requestJoinMessage = new RequestJoinMessage();
-                    requestJoinMessage.Serialize(connection, readingStream);
+                var requestJoinMessage = new RequestJoinMessage();
+                requestJoinMessage.Serialize(connection, readingStream);
 
-                    if (requestJoinMessage.PlayerMode is PlayerMode.Fly or PlayerMode.AutoCam or PlayerMode.Unassigned)
-                    {
-                        Tavern.Logger.Warning($"User kicked for bizarre mode");
-                        await ServerPlayerConnectionHandlerOld.PlayerDenied(connection, "Bizarre mode detected.");
-                        return false;
-                    }
-                }
-                catch (Exception e)
+                if (requestJoinMessage.PlayerMode is PlayerMode.Fly or PlayerMode.AutoCam or PlayerMode.Unassigned)
                 {
-                    Tavern.Logger.Error($"Error in SecurityPatch {e}");
-                    await ServerPlayerConnectionHandlerOld.PlayerDenied(connection, "Unhandled error.");
+                    Tavern.Logger.Warning($"User kicked for bizarre mode");
+                    await ServerPlayerConnectionHandlerOld.PlayerDenied(connection, "Bizarre mode detected.");
                     return false;
                 }
-            
-                ServerHandler.Current.playerJoinHandler.CheckApproved(connection, stream);
-                return true;
             }
             catch (Exception e)
             {
-                Tavern.Logger.Error($"Error when handling FilterFlyCam security patch! {e}");
-                throw;
+                Tavern.Logger.Error($"Error in SecurityPatch {e}");
+                await ServerPlayerConnectionHandlerOld.PlayerDenied(connection, "Unhandled error.");
+                return false;
             }
+
+            return true;
         }
     }
 }
