2900cf3f64fb6be017954b925e9501f87a5d4b41
Ethan Barron <ethanbarron@proton.me>
2026-07-22 08:54:43 +0100

    Generat listing token on game start

--- TavernLib/Backend/Server/Configs/ServerSettingsConfig.cs
+++ TavernLib/Backend/Server/Configs/ServerSettingsConfig.cs
@@ -11,7 +11,17 @@ public class ServerSettings
     [JsonProperty(PropertyName = "enforce_ip_limit")] public bool EnforceIpLimit { get; private set; }
     [JsonProperty(PropertyName = "community_listed")] public bool CommunityListed { get; private set; }
     [JsonProperty(PropertyName = "max_players")] public int MaxPlayers { get; private set; }
-    [JsonProperty(PropertyName = "community_listing_token")] public string CommunityListingToken { get; private set; } = "";
+    [JsonProperty(PropertyName = "community_listing_token")] public string CommunityListingToken { get; internal set; } = "";
 }
 
-public class ServerSettingsConfig(string filePath) : ServerConfigFile<ServerSettings>(filePath);
+public class ServerSettingsConfig(string filePath) : ServerConfigFile<ServerSettings>(filePath)
+{
+    public override void ReadFromFile()
+    {
+        base.ReadFromFile();
+        if (!string.IsNullOrWhiteSpace(LastRead.CommunityListingToken)) return;
+        
+        LastRead.CommunityListingToken = BackendUtils.TokenUrlSafe(24);
+        WriteToFile();
+    }
+}
