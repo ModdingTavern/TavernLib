e9d7552d3f84fbb1c7b252a773eaddde76fbc4a3
Ethan Barron <ethanbarron@proton.me>
2026-07-25 15:41:01 +0100

    SelectionFix attempt

---
+++ TavernLib/Patches/SelectFixPatch.cs
@@ -0,0 +1,14 @@
+﻿using Alta.Console.Commands;
+using HarmonyLib;
+
+namespace TavernLib.Patches;
+
+[HarmonyPatch(typeof(SelectionCommandModule))]
+public class SelectFixPatch
+{
+    [HarmonyPatch(nameof(SelectionCommandModule.FindObjects))]
+    public static void Postfix()
+    {
+        
+    }
+}
