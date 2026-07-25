ae370cbe00e903eaf8a0c6c71827156611f01420
Ethan Barron <ethanbarron@proton.me>
2026-07-25 11:32:15 +0100

    Anti RouteCommand

--- TavernLib/Patches/CommandSecurityPatches.cs
+++ TavernLib/Patches/CommandSecurityPatches.cs
@@ -1,4 +1,5 @@
 ﻿using Alta.WebServer;
+using ATT.Character.QuickAccessMenu;
 using HarmonyLib;
 
 namespace TavernLib.Patches;
@@ -10,6 +11,13 @@ public class CommandSecurityPatches
     public static bool CancelWebServerThread()
     {
         TavernLogger.Msg("Cancelling instantiation of WebServerThread");
+        return false;
+    }
+    
+    [HarmonyPatch(typeof(CommandSync), nameof(CommandSync.RouteCommand)), HarmonyPrefix]
+    public static bool CancelRouteCommand(string command) // Unsure of if this is necessary
+    {
+        TavernLogger.Msg($"Cancelling RouteCommand attempt: {command}");
         return false;
     }
     
