ffbf58a9ff3f782474d3222eb36ac51d52046754
FTWImCody <cody.mcnealy96@gmail.com>
2026-07-26 01:55:59 -0400

    Remove Internet Check
    Removed the check for connection to internet when attempting to run or join a server.

--- TavernLib/Patches/TeenyPatches.cs
+++ TavernLib/Patches/TeenyPatches.cs
@@ -262,4 +262,12 @@ public class TeenyPatches
         __result = devInfo;
     }
     #endregion
+
+    #region ApiAccess
+    [HarmonyPatch(typeof(ApiAccess), nameof(ApiAccess.IsConnectedToInternetInternal)), HarmonyPostfix]
+    public static void IsConnectedToInternetInternal_Postfix(ref bool __result)
+    {
+        __result = true;
+    }
+    #endregion
 }
