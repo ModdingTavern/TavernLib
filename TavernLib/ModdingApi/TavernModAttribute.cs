dbc700e4bc7a3fa341844a8397dddcaa1c313295
Feien <fei@feien.dev>
2026-07-28 07:30:34 +0200

    Added a custom attributes for mods to use
    I made a custom atrribute that mod makes could use to provide additional
    information about their mods that might be necessary for TavernLib to know.
    
    For now it only contains mod sides.
    Such as client sided, server sided or both.
    
    This for example can be used to later cross reference mods on servers
    such that when joining one could check if one have all the mods.
    Otherwise show an error or such.

---
+++ TavernLib/ModdingApi/TavernModAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using MelonLoader;
+
+namespace TavernLib.ModdingApi;
+
+public enum ModSide
+{
+    None,
+    Server,
+    Client,
+    Both,
+}
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false)]
+public class TavernModAttribute : Attribute
+{
+    public ModSide Side { get; }
+    public TavernModAttribute(ModSide side)
+    {
+        Side = side;
+    }
+}
+
+public static class TavernMelonBaseExtension{
+
+    public static ModSide GetModSide(this MelonBase melonMod)
+    {
+        TavernModAttribute modInfo = melonMod.GetType().GetCustomAttribute<TavernModAttribute>();
+        return modInfo?.Side ?? ModSide.None;
+    }
+}
