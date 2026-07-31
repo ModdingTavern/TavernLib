10d9d1d86c622f289ae3eef88a1ee58d76e16818
Ethan Barron <ethanbarron@proton.me>
2026-07-31 12:39:01 +0100

    Initial serverboard for roles support

--- TavernLib/Patches/ServerSettingsRolePatch.cs
+++ TavernLib/Patches/ServerSettingsRolePatch.cs
@@ -1,5 +1,7 @@
-﻿using Alta.QuickAccessActions;
+﻿using Alta.Networking;
+using Alta.QuickAccessActions;
 using HarmonyLib;
+using TavernLib.Backend;
 using TavernLib.Backend.Api;
 using TavernLib.Services;
 
@@ -11,7 +13,19 @@ public class ServerSettingsRolePatch
     [HarmonyPatch(typeof(SettingsQuickAccessMenu), nameof(SettingsQuickAccessMenu.InitializeForPlayer)), HarmonyPostfix]
     public static void ValidateUserPermissions(Player player, bool isDev, SettingsQuickAccessMenu __instance)
     {
-        __instance.hasPermission = TavernServices.GetService<TavernApiManager>().UserConfig.GetUser(player.UserInfo.Username).IsModerator;
+        // TODO: Is most of this even necessary?
+        if (NetworkSceneManager.IsServer)
+        {
+            // Allow a user to sync server settings if they're a moderator
+            __instance.hasPermission = TavernServices.GetService<TavernManager>().UserConfig.GetUser(player.UserInfo.Username).IsModerator;
+        }
+
+        else
+        {
+            __instance.hasPermission = TavernServices.GetService<EntranceMessageHandler>().LocalUser.IsModerator;
+        }
+    }
+    
     // Send a user their roles, used to toggle the quick access server board, respective of their roles
     [HarmonyPatch(typeof(Player), nameof(Player.InitializePlayerOnServer)), HarmonyPostfix]
     public static void SendRolesToUser(Player __instance)
