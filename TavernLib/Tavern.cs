a754d8c24227a45cc8d25b6864e8be6f0f8289d9
MrBanana <logande4610@gmail.com>
2026-08-02 08:54:41 -0500

    Updated custom networked items implementation for latest TavernLib release.

--- TavernLib/Tavern.cs
+++ TavernLib/Tavern.cs
@@ -1,45 +1,72 @@
 using System;
-using System.IO;
+using System.Reflection;
+using Alta.Console.Commands;
 using MelonLoader;
-using MelonLoader.Logging;
+using MonoMod.RuntimeDetour;
+using TavernLib.Backend;
 using TavernLib.Backend.Api;
 using TavernLib.Debugging;
+using TavernLib.Patches;
 using TavernLib.Services;
-using YamlDotNet.Serialization;
 
 
-[assembly: MelonInfo(typeof(TavernLib.Tavern), "TavernLib", "0.0.1", "Tavern Team", "https://github.com/ModdingTavern/TavernLib")]
-namespace TavernLib
+[assembly: MelonInfo(typeof(TavernLib.Tavern), "TavernLib", "v1.4.2", "Tavern Team", "https://github.com/ModdingTavern/TavernLib")]
+namespace TavernLib;
+
+public class Tavern : MelonPlugin
 {
-    public class Tavern : MelonPlugin
+    internal static MelonLogger.Instance Logger { get; private set; }
+    public const string Version = "v1.4.2";
+
+
+    public override void OnEarlyInitializeMelon()
     {
-        internal static MelonLogger.Instance Logger { get; private set; }
+        Logger = LoggerInstance;
+        SetupServices();
+    }
+
+    public override void OnInitializeMelon()
+    {
+        Alta.Console.CommandService.CommandCollection.Collect(Assembly.GetExecutingAssembly());
+    }
+
+    public override void OnLateInitializeMelon()
+    {
+        SetupSelectionFixPatch();
+    }
 
 
-        public override void OnEarlyInitializeMelon()
-        {
-            Logger = LoggerInstance;
-            
-            SetupServices();
-        }
+    private void SetupSelectionFixPatch()
+    {
+        var findObjectsMethod = typeof(SelectionCommandModule).GetMethod(nameof(SelectionCommandModule.FindObjects), BindingFlags.Static | BindingFlags.NonPublic);
+        var selectionFixMethod = typeof(SelectFixPatch).GetMethod(nameof(SelectFixPatch.SelectionFix), BindingFlags.Static | BindingFlags.Public);
         
-        private void SetupServices()
+        _ = new Hook(findObjectsMethod, selectionFixMethod);
+    }
+    
+    private void SetupServices()
+    {
+        try
         {
-            try
-            {
-                if (CommandLineArguments.Contains("/debug_helper")) TavernServices.AddService(new DebugHelper());
-                
-                if (CommandLineArguments.Contains(CommandLineArguments.StartServerArgument))
-                {
-                    if (!CommandLineArguments.Contains(TavernArgs.DontManageApi)) TavernServices.AddService(new TavernApiManager());
-                }
+            // Converts Alta's Real Logs Into MelonLoader Logs
+            if (CommandLineArguments.Contains("/debug_helper")) TavernServices.AddService(new DebugHelper());
 
-            }
-            catch (Exception e)
+            // Server Only Services
+            if (CommandLineArguments.Contains(CommandLineArguments.StartServerArgument))
             {
-                Logger.BigError($"Error when setting up base TavernLib services!!!!! {e}");
-                throw;
+                TavernLogger.Msg("Booting TavernLib in server mode");
+                if (!CommandLineArguments.Contains(TavernArgs.DontManageAuth)) TeenyPatches.EnsureConsoleToken();
+                
+                TavernServices.AddService(new TavernManager());
             }
+            
+            // Client & Server Services
+            TavernServices.AddService(new EntranceMessageHandler());
+        }
+        catch (Exception e)
+        {
+            Logger.BigError($"Error when setting up base TavernLib services!!!!! {e}");
+            throw;
         }
     }
 }
